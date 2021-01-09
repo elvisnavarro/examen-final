540518ebc27675602a2ad9791b82bec3d206c1e2
elvisnavarro <50973180+elvisnavarro@users.noreply.github.com>
2021-01-09 17:06:26 -0500

    feat(1a): Crear el proyecto bajo una estructura escalable y modular

---
+++ src/app/modules/ingresar/ingresar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IngresarRoutingModule } from './ingresar-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IngresarRoutingModule
+  ]
+})
+export class IngresarModule { }
