a41dcffa1f50eae9c33900e6564aa3773eb1b11e
elvisnavarro <50973180+elvisnavarro@users.noreply.github.com>
2021-01-09 18:51:19 -0500

    feat(2a): Identificar y crear las componentes de tipo contenedor

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
 import { IncioPageComponent } from './pages/incio-page/incio-page.component';
+import { CursoContainerComponent } from './components/curso-container/curso-container.component';
 
 
 @NgModule({
-  declarations: [IncioPageComponent],
+  declarations: [IncioPageComponent, CursoContainerComponent],
   imports: [
     CommonModule,
     InicioRoutingModule
