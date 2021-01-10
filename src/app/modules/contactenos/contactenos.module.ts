007b5838704e824d15a4d23ec3bb6ab54681f1ef
elvisnavarro <50973180+elvisnavarro@users.noreply.github.com>
2021-01-09 20:59:32 -0500

    feat(3b): Implementar las plantillas, formularios y métodos

--- src/app/modules/contactenos/contactenos.module.ts
+++ src/app/modules/contactenos/contactenos.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ContactenosRoutingModule } from './contactenos-routing.module';
 import { ContactenosPageComponent } from './pages/contactenos-page/contactenos-page.component';
 import { ContactenosFormComponent } from './components/contactenos-form/contactenos-form.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [ContactenosPageComponent, ContactenosFormComponent],
   imports: [
     CommonModule,
-    ContactenosRoutingModule
+    ContactenosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ContactenosModule { }
--- src/app/modules/ingresar/ingresar.module.ts
+++ src/app/modules/ingresar/ingresar.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { IngresarRoutingModule } from './ingresar-routing.module';
 import { IngresarPageComponent } from './pages/ingresar-page/ingresar-page.component';
 import { IngresarFormComponent } from './components/ingresar-form/ingresar-form.component';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 @NgModule({
   declarations: [IngresarPageComponent, IngresarFormComponent],
   imports: [
     CommonModule,
-    IngresarRoutingModule
+    IngresarRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class IngresarModule { }
